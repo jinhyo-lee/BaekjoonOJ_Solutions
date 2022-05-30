a490644e4ed4f4948b1bebf4af76825df6d8de56
Jin <jinhyo.dev@gmail.com>
2022-05-31 02:11:06 +0900

    Add 2290

--- BaekjoonOJ/Implementation/2290.java
+++ BaekjoonOJ/Implementation/2290.java
@@ -1,78 +1,103 @@
-package test;
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int s, n;
-	static int left;
-	static String[][] board;
-	static int[][] map = { { 0, 1, 1, 1, 0, 1, 1, 1 }, 
-						   { 0, 0, 0, 1, 0, 0, 1, 0 }, 
-						   { 0, 1, 0, 1, 1, 1, 0, 1 },
-						   { 0, 1, 0, 1, 1, 0, 1, 1 }, 
-						   { 0, 0, 1, 1, 1, 0, 1, 0 }, 
-						   { 0, 1, 1, 0, 1, 0, 1, 1 },
-						   { 0, 1, 1, 0, 1, 1, 1, 1 }, 
-						   { 0, 1, 0, 1, 0, 0, 1, 0 }, 
-						   { 0, 1, 1, 1, 1, 1, 1, 1 },
-						   { 0, 1, 1, 1, 1, 0, 1, 1 } };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		s = Integer.parseInt(st.nextToken());
-		String[] num = st.nextToken().split("");
-		n = num.length;
-		
-		board = new String[2 * s + 4][n * (s + 4)];
-		for (int i = 0; i < 2 * s + 3; i++)
-			for (int j = 0; j < n * (s + 4); j++)
-				board[i][j] = " ";
+		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < n; i++) {
-			int n = Integer.parseInt(num[i]);
-			if (map[n][1] == 1)
-				fill(0, 0, left + 1, left + s, "-");
-			if (map[n][4] == 1)
-				fill(s + 1, s + 1, left + 1, left + s, "-");
-			if (map[n][7] == 1)
-				fill(2 * (s + 1), 2 * (s + 1), left + 1, left + s, "-");
+		int s = Integer.parseInt(st.nextToken());
+		String n = st.nextToken();
+		int len = n.length();
 
-			if (map[n][2] == 1)
-				fill(1, s, left, left, "|");
-			if (map[n][5] == 1)
-				fill(1 + (s + 1), s + (s + 1), left, left, "|");
-
-			if (map[n][3] == 1)
-				fill(1, s, left + s + 1, left + s + 1, "|");
-			if (map[n][6] == 1)
-				fill(1 + (s + 1), s + (s + 1), left + s + 1, left + s + 1, "|");
-
-			left += s + 3;
-		}
-
-		result();
-		br.close();
-	}
-
-	public static void result() {
-		for (int i = 0; i < 2 * s + 3; i++) {
-			for (int j = 0; j < left; j++) {
-				System.out.print(board[i][j]);
+		for (int i = 0; i < len; i++) {
+			sb.append(' ');
+			for (int j = 0; j < s; j++) {
+				if (n.charAt(i) == '1' || n.charAt(i) == '4')
+					sb.append(' ');
+				else
+					sb.append('-');
 			}
-			System.out.println();
+			sb.append(' ');
+			sb.append(' ');
 		}
-	}
+		sb.append('\n');
 
-	public static void fill(int sx, int ex, int sy, int ey, String str) {
-		for (int x = sx; x <= ex; x++)
-			for (int y = sy; y <= ey; y++)
-				board[x][y] = str;
+		for (int i = 0; i < s; i++) {
+			for (int j = 0; j < len; j++) {
+				if (n.charAt(j) == '1' || n.charAt(j) == '2' || n.charAt(j) == '3' || n.charAt(j) == '7')
+					sb.append(' ');
+				else
+					sb.append('|');
+
+				for (int k = 0; k < s; k++)
+					sb.append(' ');
+
+				if (n.charAt(j) == '5' || n.charAt(j) == '6')
+					sb.append(' ');
+				else
+					sb.append('|');
+
+				sb.append(' ');
+			}
+			sb.append('\n');
+		}
+
+		for (int i = 0; i < len; i++) {
+			sb.append(' ');
+			for (int j = 0; j < s; j++) {
+				if (n.charAt(i) == '1' || n.charAt(i) == '7' || n.charAt(i) == '0')
+					sb.append(' ');
+				else
+					sb.append('-');
+			}
+			sb.append(' ');
+			sb.append(' ');
+		}
+		sb.append('\n');
+
+		for (int i = 0; i < s; i++) {
+			for (int j = 0; j < len; j++) {
+				if (n.charAt(j) == '2' || n.charAt(j) == '6' || n.charAt(j) == '8' || n.charAt(j) == '0')
+					sb.append('|');
+				else
+					sb.append(' ');
+
+				for (int k = 0; k < s; k++)
+					sb.append(' ');
+
+				if (n.charAt(j) == '2')
+					sb.append(' ');
+				else
+					sb.append('|');
+
+				sb.append(' ');
+			}
+			sb.append('\n');
+		}
+
+		for (int i = 0; i < len; i++) {
+			sb.append(' ');
+			for (int j = 0; j < s; j++) {
+				if (n.charAt(i) == '1' || n.charAt(i) == '4' || n.charAt(i) == '7')
+					sb.append(' ');
+				else
+					sb.append('-');
+			}
+			sb.append(' ');
+			sb.append(' ');
+		}
+		sb.append('\n');
+		
+		bw.write(sb + "");
+		bw.flush();
 	}
 
 }
