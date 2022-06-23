60757e45a9d933576942eda066844fe1b80f935c
Jin <jinhyo.dev@gmail.com>
2022-06-23 20:43:37 +0900

    Add 14499

---
+++ BaekjoonOJ/Implementation/14499.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int n, m, y, x;
+	static int[][] map;
+	static int[] dice = new int[6];
+	static int[] dy = { 0, 0, -1, 1 };
+	static int[] dx = { 1, -1, 0, 0 };
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		map = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < m; j++)
+				map[i][j] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < k; i++) {
+			int dir = Integer.parseInt(st.nextToken()) - 1;
+			y += dy[dir];
+			x += dx[dir];
+
+			if (y < 0 || x < 0 || y >= n || x >= m) {
+				y -= dy[dir];
+				x -= dx[dir];
+				continue;
+			}
+
+			bw.write(move(dir + 1, y, x) + "\n");
+		}
+
+		bw.flush();
+	}
+
+	private static int move(int dir, int y, int x) {
+		int tmp = dice[0];
+
+		switch (dir) {
+		case 1:
+			dice[0] = dice[4];
+			dice[4] = dice[1];
+			dice[1] = dice[5];
+			dice[5] = tmp;
+			break;
+		case 2:
+			dice[0] = dice[5];
+			dice[5] = dice[1];
+			dice[1] = dice[4];
+			dice[4] = tmp;
+			break;
+		case 3:
+			dice[0] = dice[2];
+			dice[2] = dice[1];
+			dice[1] = dice[3];
+			dice[3] = tmp;
+			break;
+		case 4:
+			dice[0] = dice[3];
+			dice[3] = dice[1];
+			dice[1] = dice[2];
+			dice[2] = tmp;
+			break;
+		}
+
+		if (map[y][x] == 0)
+			map[y][x] = dice[1];
+		else {
+			dice[1] = map[y][x];
+			map[y][x] = 0;
+		}
+
+		return dice[0];
+	}
+
+}
