523c00f3773ec2238814a54e7c9b1cf24bb710fe
Jin <jinhyo.dev@gmail.com>
2022-06-29 19:42:52 +0900

    Add 1260

--- BaekjoonOJ/GraphTraversal/1260.java
+++ BaekjoonOJ/GraphTraversal/1260.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -9,14 +11,15 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class P1260 {
 
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb = new StringBuilder();
 	static ArrayList<ArrayList<Integer>> list;
-	static boolean[] check;
+	static boolean[] visit;
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int n = Integer.parseInt(st.nextToken());
@@ -24,9 +27,8 @@ public class Main {
 		int v = Integer.parseInt(st.nextToken());
 
 		list = new ArrayList<>();
-		for (int i = 0; i < n + 1; i++) {
+		for (int i = 0; i < n + 1; i++)
 			list.add(new ArrayList<>());
-		}
 
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -37,46 +39,45 @@ public class Main {
 			list.get(y).add(x);
 		}
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++)
 			Collections.sort(list.get(i));
-		}
 
-		check = new boolean[n + 1];
+		visit = new boolean[n + 1];
 		dfs(v);
-		bw.write("\n");
+		sb.append("\n");
 
-		check = new boolean[n + 1];
+		visit = new boolean[n + 1];
 		bfs(v);
 
+		bw.write(sb + "\n");
 		bw.flush();
 	}
 
-	public static void dfs(int visit) throws IOException {
-		if (check[visit])
+	public static void dfs(int node) throws IOException {
+		if (visit[node])
 			return;
 
-		check[visit] = true;
-		bw.write(visit + " ");
+		visit[node] = true;
+		sb.append(node + " ");
 
-		for (int i : list.get(visit)) {
-			if (!check[i]) {
+		for (int i : list.get(node)) {
+			if (!visit[i])
 				dfs(i);
-			}
 		}
 	}
 
-	public static void bfs(int start) throws IOException {
+	public static void bfs(int node) throws IOException {
 		Queue<Integer> q = new LinkedList<>();
-		q.offer(start);
-		check[start] = true;
+		q.offer(node);
+		visit[node] = true;
 
 		while (!q.isEmpty()) {
-			int visit = q.poll();
-			bw.write(visit + " ");
+			int next = q.poll();
+			sb.append(next + " ");
 
-			for (int i : list.get(visit)) {
-				if (!check[i]) {
-					check[i] = true;
+			for (int i : list.get(next)) {
+				if (!visit[i]) {
+					visit[i] = true;
 					q.offer(i);
 				}
 			}
