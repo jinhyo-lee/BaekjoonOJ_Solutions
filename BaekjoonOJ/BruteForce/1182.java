3c54ec22a4eed2fdcae393afd2ec6faf83c9515d
Jin <jinhyo.dev@gmail.com>
2022-07-04 20:17:56 +0900

    Add 1182

---
+++ BaekjoonOJ/BruteForce/1182.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+
+		int cnt = 0;
+		for (int i = 1; i < (1 << n); i++) {
+			int sum = 0;
+			for (int j = 0; j < n; j++) {
+				if ((i & (1 << j)) != 0)
+					sum += arr[j];
+			}
+
+			if (sum == s)
+				cnt++;
+		}
+
+		bw.write(cnt + "\n");
+		bw.flush();
+	}
+
+}
