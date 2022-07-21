207681277d8ccde4eb23f0cb1ccfed08b5dd0b42
Jin <jinhyo.dev@gmail.com>
2022-07-21 22:40:39 +0900

    Add 11053

--- BaekjoonOJ/DynamicProgramming/11053.java
+++ BaekjoonOJ/DynamicProgramming/11053.java
@@ -20,18 +20,17 @@ public class Main {
 		for (int i = 0; i < n; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
 		
+		int max = 0;
 		for (int i = 0; i < n; i++) {
 			dp[i] = 1;
 			for (int j = 0; j < i; j++) {
 				if (arr[j] < arr[i] && dp[i] <= dp[j])
 					dp[i] = dp[j] + 1;
 			}
+			
+			max = Math.max(max, dp[i]);	
 		}
-		
-		int max = 0;
-		for (int i = 0; i < n; i++) 
-			max = Math.max(max, dp[i]);		
-		
+
 		bw.write(max + "\n");
 		bw.flush();
 	}
