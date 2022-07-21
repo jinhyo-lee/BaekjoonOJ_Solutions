0c7ee57f9471edc9e0ceb99643f0b738a55450f7
Jin <jinhyo.dev@gmail.com>
2022-07-21 22:24:22 +0900

    Add 11053

---
+++ BaekjoonOJ/DynamicProgramming/11053.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < n; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] < arr[i] && dp[i] <= dp[j])
+					dp[i] = dp[j] + 1;
+			}
+		}
+		
+		int max = 0;
+		for (int i = 0; i < n; i++) 
+			max = Math.max(max, dp[i]);		
+		
+		bw.write(max + "\n");
+		bw.flush();
+	}
+
+}
