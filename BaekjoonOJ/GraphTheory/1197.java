d3a996be7255c5ea77870a7ba2c82a8b87a20a8c
Jin <jinhyo.dev@gmail.com>
2022-08-16 19:28:11 +0900

    Add 1197

---
+++ BaekjoonOJ/GraphTheory/1197.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        parent = new int[v + 1];
+        for (int i = 1; i <= v; i++)
+            parent[i] = i;
+
+        List<Edge> list = new ArrayList<>();
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int src = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            list.add(new Edge(src, dest, weight));
+        }
+
+        Collections.sort(list);
+
+        int cost = 0;
+        for (int i = 0; i < e; i++) {
+            Edge edge = list.get(i);
+
+            if (find(edge.src) != find(edge.dest)) {
+                cost += edge.weight;
+                union(edge.src, edge.dest);
+            }
+        }
+
+        bw.write(cost + "\n");
+        bw.flush();
+    }
+
+    private static int find(int node) {
+        if (node == parent[node])
+            return node;
+
+        return parent[node] = find(parent[node]);
+    }
+
+    private static void union(int src, int dest) {
+        src = find(src);
+        dest = find(dest);
+
+        if (src != dest)
+            parent[dest] = src;
+    }
+
+    private static class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+
+        public Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return weight - o.weight;
+        }
+    }
+
+}
