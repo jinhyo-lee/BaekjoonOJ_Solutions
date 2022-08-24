c251f07ca2f743eeaa9a7f1e3213e633fd9ed2bc
Jin <jinhyo.dev@gmail.com>
2022-08-24 19:30:04 +0900

    Add 2193

---
+++ BaekjoonOJ/DynamicProgramming/2193.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][2];
+
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
+        }
+
+        bw.write(dp[n][0] + dp[n][1] + "\n");
+        bw.flush();
+    }
+
+}
