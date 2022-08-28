3abe98d952a5bfd709d22e1588d44e49fa1c6f5b
Jin <jinhyo.dev@gmail.com>
2022-08-28 19:40:21 +0900

    Add 1149

---
+++ BaekjoonOJ/DynamicProgramming/1149.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][3];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int red = Integer.parseInt(st.nextToken());
+            int green = Integer.parseInt(st.nextToken());
+            int blue = Integer.parseInt(st.nextToken());
+
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + red;
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + green;
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + blue;
+        }
+
+        bw.write(Math.min(dp[n][0], Math.min(dp[n][1], dp[n][2])) + "\n");
+        bw.flush();
+    }
+
+}
