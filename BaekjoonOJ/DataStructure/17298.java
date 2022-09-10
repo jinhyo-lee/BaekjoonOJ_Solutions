1ed27278c9c0d8330a3bb89d9a1aceef83ba0d08
Jin <jinhyo.dev@gmail.com>
2022-09-10 16:22:53 +0900

    Add 17298

---
+++ BaekjoonOJ/DataStructure/17298.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && arr[stk.peek()] < arr[i])
+                arr[stk.pop()] = arr[i];
+
+            stk.push(i);
+        }
+
+        while (!stk.empty())
+            arr[stk.pop()] = -1;
+
+        for (int i = 0; i < n; i++)
+            bw.write(arr[i] + " ");
+
+        bw.flush();
+    }
+
+}
