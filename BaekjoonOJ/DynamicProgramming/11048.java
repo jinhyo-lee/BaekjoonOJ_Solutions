13f5e862e7e94b9a574c5b64274aa091a8372e99
Jin <jinhyo.dev@gmail.com>
2022-10-17 22:52:42 +0900

    Add 11048

---
+++ BaekjoonOJ/DynamicProgramming/11048.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] += Math.max(map[i][j - 1], Math.max(map[i - 1][j - 1], map[i - 1][j]));
+            }
+        }
+
+        bw.write(String.valueOf(map[n][m]));
+        bw.flush();
+    }
+
+}
