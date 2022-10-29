99e3c94464df3a5f8fe7f845e0ac5dda3ec3bb1c
Jin <jinhyo.dev@gmail.com>
2022-10-29 17:59:08 +0900

    Add 1956

---
+++ BaekjoonOJ/GraphTheory/1956.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[v + 1][v + 1];
+        int max = 4000001;
+
+        for (int i = 1; i <= v; i++)
+            Arrays.fill(dist[i], max);
+
+        for (int i = 0; i < e; i++) {
+            st = new StringTokenizer(br.readLine());
+            int src = Integer.parseInt(st.nextToken());
+            int dest = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            dist[src][dest] = weight;
+        }
+
+        for (int k = 1; k <= v; k++) {
+            for (int i = 1; i <= v; i++) {
+                for (int j = 1; j <= v; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+
+        for (int i = 1; i <= v; i++)
+            max = Math.min(dist[i][i], max);
+
+        bw.write(String.valueOf(max == 4000001 ? -1 : max));
+        bw.flush();
+    }
+
+}
