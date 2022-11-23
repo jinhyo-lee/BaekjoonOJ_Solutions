abacfb59f6e4a85ec008e3fc9df5dd87fb494b9f
Jin <jinhyo.dev@gmail.com>
2022-11-23 17:51:39 +0900

    Add 1926

---
+++ BaekjoonOJ/GraphTheory/1926.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m, max, size;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[] dy = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visit = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visit[i][j] && map[i][j] != 0) {
+                    size = 1;
+                    dfs(i, j);
+
+                    cnt++;
+                    max = size > max ? size : max;
+                }
+            }
+        }
+
+        bw.write(cnt + "\n" + max);
+        bw.flush();
+    }
+
+    private static void dfs(int y, int x) {
+        visit[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m)
+                continue;
+            if (!visit[ny][nx] && map[ny][nx] != 0) {
+                size++;
+                dfs(ny, nx);
+            }
+        }
+    }
+
+}
