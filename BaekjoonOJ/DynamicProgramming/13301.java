18ea86ccaa931c706ab049070bba28aca89cbbff
Jin <jinhyo.dev@gmail.com>
2023-01-13 07:02:33 +0900

    Add 13301

---
+++ BaekjoonOJ/DynamicProgramming/13301.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[n + 1];
+        dp[0] = 2;
+        dp[1] = 4;
+
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+    }
+
+}
