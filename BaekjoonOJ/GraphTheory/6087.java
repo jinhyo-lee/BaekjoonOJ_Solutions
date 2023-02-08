8aca779786c2a8471eefd37defc662f567f7c5ef
Jin <jinhyo.dev@gmail.com>
2023-02-08 22:12:28 +0900

    Add 6087

--- BaekjoonOJ/GraphTheory/6087.java
+++ BaekjoonOJ/GraphTheory/6087.java
@@ -3,16 +3,14 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int w, h, res;
+    static int w, h;
     static char[][] map;
-    static int[][] visit;
+    static int[][][] visit;
     static int[] dy = {-1, 1, 0, 0};
     static int[] dx = {0, 0, -1, 1};
     static Node src, dest;
@@ -26,42 +24,38 @@ public class Main {
         h = Integer.parseInt(st.nextToken());
 
         map = new char[h][w];
-        visit = new int[h][w];
+        visit = new int[h][w][4];
 
-        boolean flag = false;
         for (int i = 0; i < h; i++) {
             map[i] = br.readLine().toCharArray();
             for (int j = 0; j < w; j++) {
+                for (int k = 0; k < 4; k++)
+                    visit[i][j][k] = Integer.MAX_VALUE;
+
                 if (map[i][j] == 'C') {
-                    if (!flag) {
+                    if (src == null)
                         src = new Node(i, j, -1, 0);
-                        flag = true;
-                    } else
+                    else
                         dest = new Node(i, j, -1, 0);
                 }
             }
         }
 
-        for (int i = 0; i < h; i++)
-            Arrays.fill(visit[i], h * w);
-
-        bfs();
-
-        bw.write(String.valueOf(res));
+        bw.write(String.valueOf(bfs()));
         bw.flush();
     }
 
-    private static void bfs() {
-        Queue<Node> q = new PriorityQueue<>((o1, o2) -> o1.cnt - o2.cnt);
-        q.offer(src);
-        visit[src.y][src.x] = 0;
+    private static int bfs() {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(src);
 
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
+        int min = Integer.MAX_VALUE;
+        while (!pq.isEmpty()) {
+            Node cur = pq.poll();
 
             if (cur.y == dest.y && cur.x == dest.x) {
-                res = cur.cnt;
-                return;
+                min = Math.min(min, cur.cnt);
+                continue;
             }
 
             for (int i = 0; i < 4; i++) {
@@ -75,16 +69,18 @@ public class Main {
                 if (cur.dir != i && cur.dir != -1)
                     nCnt++;
 
-                if (visit[ny][nx] < nCnt)
+                if (visit[ny][nx][i] <= nCnt)
                     continue;
 
-                visit[ny][nx] = nCnt;
-                q.offer(new Node(ny, nx, i, nCnt));
+                visit[ny][nx][i] = nCnt;
+                pq.offer(new Node(ny, nx, i, nCnt));
             }
         }
+
+        return min;
     }
 
-    private static class Node {
+    private static class Node implements Comparable<Node> {
         int y, x, dir, cnt;
 
         public Node(int y, int x, int dir, int cnt) {
@@ -92,6 +88,11 @@ public class Main {
             this.x = x;
             this.dir = dir;
             this.cnt = cnt;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return cnt - o.cnt;
         }
     }
 
