60636513c2cfda03df19386e7fc2d82f36753f57
Jin <jinhyo.dev@gmail.com>
2023-03-25 16:18:59 +0900

    Add 18353

--- BaekjoonOJ/DynamicProgramming/18353.java
+++ BaekjoonOJ/DynamicProgramming/18353.java
@@ -21,9 +21,6 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
             dp[i] = 1;
-        }
-
-        for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (arr[i] < arr[j])
                     dp[i] = Math.max(dp[i], dp[j] + 1);
