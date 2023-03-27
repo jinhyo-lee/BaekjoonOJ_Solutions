b8bbc4a6585033897b101100478efe5c228eb073
Jin <jinhyo.dev@gmail.com>
2023-03-27 14:27:13 +0900

    Add 14430

---
+++ BaekjoonOJ/DynamicProgramming/14430.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= m; j++)
+                dp[i][j] = Integer.parseInt(st.nextToken()) + Math.max(dp[i - 1][j], dp[i][j - 1]);
+        }
+
+        bw.write(String.valueOf(dp[n][m]));
+        bw.flush();
+    }
+
+}
