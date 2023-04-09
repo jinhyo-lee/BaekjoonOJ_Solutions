a2be0f32bb4e84f5d31157e712a46db79d198f4f
Jin <jinhyo.dev@gmail.com>
2023-04-09 12:24:26 +0900

    Add 5545

---
+++ BaekjoonOJ/Greedy/5545.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        int cal = c, cost = a, max = c / a;
+        for (int i = n - 1; i >= 0; i--) {
+            cal += arr[i];
+            cost += b;
+
+            if (cal / cost >= max)
+                max = cal / cost;
+            else
+                break;
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+
+}
