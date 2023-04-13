4ada9561e42470b1a94acfe45d474dd8363e0f1b
Jin <jinhyo.dev@gmail.com>
2023-04-13 23:22:58 +0900

    Add 20186

---
+++ BaekjoonOJ/Greedy/20186.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for (int i = n - 1, j = 0; i >= n - k; i--, j++)
+            sum += arr[i] - j;
+
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+
+}
