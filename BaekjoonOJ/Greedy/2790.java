a35730b5b0a8359897b83297607acdcd9e2de005
Jin <jinhyo.dev@gmail.com>
2023-05-11 20:20:24 +0900

    Add 2790

---
+++ BaekjoonOJ/Greedy/2790.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        int score = arr[n - 1], cnt = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] + n < score)
+                break;
+
+            cnt++;
+            score = Math.max(score, arr[i] + n - i);
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+}
