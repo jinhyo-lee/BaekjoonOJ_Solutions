ea8fc645d1cde967cb610681b550828d1700be70
Jin <jinhyo.dev@gmail.com>
2023-08-16 19:20:30 +0900

    Add 25400

---
+++ BaekjoonOJ/Greedy/25400.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), t = n;
+
+        int i = 1;
+        while (t-- > 0) if (read() == i) i++;
+
+        bw.write(String.valueOf(n - i + 1));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
