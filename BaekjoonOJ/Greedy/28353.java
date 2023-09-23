3e47c8e729dfc3f051b777acd78b46e8546b2ccc
Jin <jinhyo.dev@gmail.com>
2023-09-23 15:17:42 +0900

    Add 28353

---
+++ BaekjoonOJ/Greedy/28353.java
@@ -0,0 +1,39 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), k = read();
+
+        int[] arr = new int[n];
+        while (n-- > 0) arr[n] = read();
+
+        Arrays.sort(arr);
+
+        int cnt = 0, i = 0, j = arr.length - 1;
+        while (i < j) {
+            if (arr[i] + arr[j] > k) j--;
+            else {
+                i++;
+                j--;
+                cnt++;
+            }
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
