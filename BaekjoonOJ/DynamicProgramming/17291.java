b781fbc992515e4754c2899b64e4d4448ba7652b
Jin <jinhyo.dev@gmail.com>
2023-09-28 15:00:25 +0900

    Add 17291

---
+++ BaekjoonOJ/DynamicProgramming/17291.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read();
+
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] * 2;
+            if (i % 2 == 0) dp[i] -= (i - 4 > -1 ? dp[i - 4] : 0) + (i - 5 > -1 ? dp[i - 5] : 0);
+        }
+
+        bw.write(String.valueOf(dp[n]));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
