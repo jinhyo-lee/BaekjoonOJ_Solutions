496240c9675e9e79b159448096abf6ae23c29d4d
Jin <jinhyo.dev@gmail.com>
2023-10-21 17:08:16 +0900

    Add 13170

---
+++ BaekjoonOJ/Greedy/13170.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        read();
+        read();
+        int p = read(), w = read();
+
+        bw.write(String.valueOf((p - 1) / w + 1));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
