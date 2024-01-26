3e12dc4dffcd33d640d45260ba788670e62d10f7
Jin <jinhyo.dev@gmail.com>
2024-01-26 15:09:02 +0900

    Add 16561

---
+++ BaekjoonOJ/BruteForce/16561.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), div = n / 3, cnt = 0;
+        for (int i = 1; i < div; i++)
+            for (int sub = i * 3, j = 1; j < div; j++)
+                if (n - (sub + j * 3) >= 3) cnt++;
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
