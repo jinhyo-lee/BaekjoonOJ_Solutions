9eb52bd6abbe4a895e17dc1fb5afa711884db122
Jin <jinhyo.dev@gmail.com>
2024-03-19 16:31:55 +0900

    Add 10809

---
+++ BaekjoonOJ/Implementation/10809.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+        for (char c : new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'})
+            sb.append(s.indexOf(c)).append(" ");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
