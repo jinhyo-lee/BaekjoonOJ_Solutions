d869acbe70f51f66d7a34254b66bfd0c184d1ced
Jin <jinhyo.dev@gmail.com>
2024-03-30 16:14:53 +0900

    Add 10815

---
+++ BaekjoonOJ/DataStructure/10815.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        Set<Integer> set = new HashSet<>();
+        int n = read();
+        while (n-- > 0) set.add(read());
+
+        int m = read();
+        while (m-- > 0) sb.append(set.contains(read()) ? "1 " : "0 ");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
