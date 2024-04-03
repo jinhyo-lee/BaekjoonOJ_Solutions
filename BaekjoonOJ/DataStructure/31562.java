f8a8f94db03aee5fe5a78ccb9be28d6899a8393d
Jin <jinhyo.dev@gmail.com>
2024-04-03 18:35:37 +0900

    Add 31562

--- BaekjoonOJ/DataStructure/31562.java
+++ BaekjoonOJ/DataStructure/31562.java
@@ -3,9 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
@@ -19,20 +17,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
 
-        Map<String, List<String>> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         while (n-- > 0) {
             String[] arr = br.readLine().split(" ", 3);
             String key = arr[2].substring(0, 5);
 
-            List<String> list = map.getOrDefault(key, new ArrayList<>());
-            list.add(arr[1]);
-            map.put(key, list);
+            map.put(key, map.containsKey(key) ? "?\n" : arr[1] + "\n");
         }
 
-        while (m-- > 0) {
-            List<String> list = map.getOrDefault(br.readLine(), new ArrayList<>());
-            sb.append(list.isEmpty() ? "!\n" : list.size() == 1 ? list.get(0) + "\n" : "?\n");
-        }
+        while (m-- > 0) sb.append(map.getOrDefault(br.readLine(), "!\n"));
 
         bw.write(sb.toString());
         bw.flush();
