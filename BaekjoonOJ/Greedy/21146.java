06c3e40f9acd092304e2e576b883b0a13d93b006
Jin <jinhyo.dev@gmail.com>
2024-04-13 12:31:05 +0900

    Add 21146

---
+++ BaekjoonOJ/Greedy/21146.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), k = read();
+
+        float sum = 0;
+        for (int i = 0; i < k; i++) sum += read();
+
+        bw.write((sum + -3 * (n - k)) / n + " " + (sum + 3 * (n - k)) / n);
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
