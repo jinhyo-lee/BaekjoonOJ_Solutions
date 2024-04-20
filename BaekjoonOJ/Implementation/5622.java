6e2a0c92586ac836ae2224a2d4944636f454e435
Jin <jinhyo.dev@gmail.com>
2024-04-20 14:59:19 +0900

    Add 5622

---
+++ BaekjoonOJ/Implementation/5622.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = {3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
+
+        int cnt = 0;
+        for (char c : br.readLine().toCharArray()) cnt += arr[c - 'A'];
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+}
