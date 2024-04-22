213b1e87245b6b8d82d6dfa93cfbb6af08320340
Jin <jinhyo.dev@gmail.com>
2024-04-22 15:11:05 +0900

    Add 9873

---
+++ BaekjoonOJ/BruteForce/9873.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = read();
+
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        for (int i = 0; i < n - 2; i++)
+            for (int j = i + 1; j < n - 1; j++)
+                for (int k = j + 1; k < n; k++)
+                    if (arr[j] - arr[i] <= arr[k] - arr[j] && (arr[j] - arr[i]) * 2 >= arr[k] - arr[j]) cnt++;
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
