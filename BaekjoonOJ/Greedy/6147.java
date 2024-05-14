d9b073338039999ce0820308a76438d157a9b969
Jin <jinhyo.dev@gmail.com>
2024-05-14 11:19:41 +0900

    Add 6147

---
+++ BaekjoonOJ/Greedy/6147.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), b = read();
+        int[] arr = new int[n];
+        while (n-- > 0) arr[n] = read();
+
+        Arrays.sort(arr);
+
+        int i = arr.length;
+        while (b > 0) b -= arr[--i];
+
+        bw.write(String.valueOf(arr.length - i));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
