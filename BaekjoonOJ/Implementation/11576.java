001181d18ee059c438a6c2d0f4aeb2091897bf91
Jin <jinhyo.dev@gmail.com>
2024-06-30 13:29:34 +0900

    Add 11576

---
+++ BaekjoonOJ/Implementation/11576.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int a = read(), b = read(), m = read(), n = 0;
+        while (m-- > 0) n += (int) (read() * Math.pow(a, m));
+
+        do sb.insert(0, n % b + " "); while ((n /= b) > 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
