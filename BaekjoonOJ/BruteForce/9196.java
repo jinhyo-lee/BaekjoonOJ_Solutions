1ae8c0a3a87c2f382ca9bde7348f26749d8140e8
Jin <jinhyo.dev@gmail.com>
2024-07-06 14:48:52 +0900

    Add 9196

--- BaekjoonOJ/BruteForce/9196.java
+++ BaekjoonOJ/BruteForce/9196.java
@@ -1,33 +1,40 @@
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
+
+    static List<Pair> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        Queue<Pair> pq = new PriorityQueue<>();
-        int h, w, d, o;
+        for (int i = 1; i < 151; i++) for (int j = i + 1; j < 151; j++) list.add(new Pair(i, j));
+        Collections.sort(list);
+
+        int h;
         while ((h = read()) != 0) {
-            w = read();
-            d = calc(h, w);
-
-            for (int i = 1; i < 151; i++)
-                for (int j = i + 1; j < 151; j++)
-                    if ((o = calc(i, j)) > d || (o == d && i > h)) pq.offer(new Pair(i, j));
-
-            Pair p = pq.poll();
-            assert p != null;
+            Pair p = find(h, read());
             sb.append(p.h).append(" ").append(p.w).append("\n");
-            pq.clear();
         }
 
         bw.write(sb.toString());
         bw.flush();
+    }
+
+    private static Pair find(int h, int w) {
+        Iterator<Pair> it = list.iterator();
+        while (it.hasNext()) {
+            Pair p = it.next();
+            if (p.h == h && p.w == w) return it.next();
+        }
+
+        return new Pair(h, w);
     }
 
     private static class Pair implements Comparable<Pair> {
@@ -36,17 +43,13 @@ public class Main {
         Pair(int h, int w) {
             this.h = h;
             this.w = w;
-            this.d = calc(h, w);
+            this.d = h * h + w * w;
         }
 
         @Override
         public int compareTo(Pair o) {
             return this.d != o.d ? this.d - o.d : this.h - o.h;
         }
-    }
-
-    private static int calc(int h, int w) {
-        return h * h + w * w;
     }
 
     private static int read() throws IOException {
