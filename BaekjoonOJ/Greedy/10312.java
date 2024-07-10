2c0bbf8c764d55cc67a5552accfded0089ae9b40
Jin <jinhyo.dev@gmail.com>
2024-07-10 10:13:56 +0900

    Add 10312

---
+++ BaekjoonOJ/Greedy/10312.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = read();
+        while (n-- > 0) {
+            StringBuilder tc = new StringBuilder();
+            int k = read();
+            do tc.insert(0, k % 3 + " "); while ((k /= 3) > 0);
+            sb.append(tc).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
