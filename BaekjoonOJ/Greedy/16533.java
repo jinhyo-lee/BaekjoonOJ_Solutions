5446a952a9adef5de988db362df3c8971bc85651
Jin <jinhyo.dev@gmail.com>
2024-07-12 17:19:26 +0900

    Add 16533

---
+++ BaekjoonOJ/Greedy/16533.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), prev = read(), cur, cnt = 1;
+        while (n-- > 1) {
+            if ((cur = read()) > prev) cnt++;
+            prev = cur;
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
