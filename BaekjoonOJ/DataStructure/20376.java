c0abe814c26725402b9243b14eb676d8ad1975cb
Jin <jinhyo.dev@gmail.com>
2024-07-22 15:19:21 +0900

    Add 20376

---
+++ BaekjoonOJ/DataStructure/20376.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<String> set = new HashSet<>();
+        String s;
+        while ((s = br.readLine()) != null) set.add(s.substring(11));
+
+        bw.write(String.valueOf(set.size()));
+        bw.flush();
+    }
+
+}
