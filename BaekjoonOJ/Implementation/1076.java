2b753fd871a568789a18af2de797a9c579dc9898
Jin <jinhyo.dev@gmail.com>
2024-08-28 13:43:05 +0900

    Add 1076

---
+++ BaekjoonOJ/Implementation/1076.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Map<String, Integer> map = init();
+        long l = (long) ((map.get(br.readLine()) * 10 + map.get(br.readLine())) * Math.pow(10, map.get(br.readLine())));
+
+        bw.write(String.valueOf(l));
+        bw.flush();
+    }
+
+    private static Map<String, Integer> init() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("black", 0);
+        map.put("brown", 1);
+        map.put("red", 2);
+        map.put("orange", 3);
+        map.put("yellow", 4);
+        map.put("green", 5);
+        map.put("blue", 6);
+        map.put("violet", 7);
+        map.put("grey", 8);
+        map.put("white", 9);
+
+        return map;
+    }
+
+}
