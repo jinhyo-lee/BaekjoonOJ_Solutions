88d629fc5de4184696f53c8789d3fd3a0ba23752
Jin <jinhyo.dev@gmail.com>
2024-09-20 16:47:37 +0900

    Add 9094

---
+++ BaekjoonOJ/BruteForce/9094.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = read();
+        while (t-- > 0) {
+            int n = read(), m = read(), cnt = 0;
+            for (int i = 1; i < n; i++) for (int j = i + 1; j < n; j++) if ((i * i + j * j + m) % (i * j) == 0) cnt++;
+            sb.append(cnt).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
