bbb99ca8f66144942919ee69333383489eddd434
Jin <jinhyo.dev@gmail.com>
2024-09-22 16:56:07 +0900

    Add 13225

---
+++ BaekjoonOJ/BruteForce/13225.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int c = read();
+        while (c-- > 0) {
+            int n = read(), cnt = 0;
+            for (int i = 1; i <= n; i++) if (n % i == 0) cnt++;
+            sb.append(n).append(" ").append(cnt).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
