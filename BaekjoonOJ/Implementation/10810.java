08b9f32c78db9eda7e6d3467ee6ce9d286d963bd
Jin <jinhyo.dev@gmail.com>
2024-09-25 18:01:32 +0900

    Add 10810

---
+++ BaekjoonOJ/Implementation/10810.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[read()];
+        int m = read();
+        while (m-- > 0) {
+            int i = read(), j = read(), k = read();
+            while (j-- >= i) arr[j] = k;
+        }
+
+        for (int i : arr) sb.append(i).append(" ");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
