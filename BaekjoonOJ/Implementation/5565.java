e5d731f8bc6a76448f59cf794f28301a45d4e060
Jin <jinhyo.dev@gmail.com>
2024-09-26 22:03:02 +0900

    Add 5565

---
+++ BaekjoonOJ/Implementation/5565.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read();
+        for (int i = 0; i < 9; i++) n -= read();
+
+        bw.write(String.valueOf(n));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
