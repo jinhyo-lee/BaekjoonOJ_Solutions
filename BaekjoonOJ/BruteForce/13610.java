5f8e99e35e56a793f9f85a9af98cb9518b30f240
Jin <jinhyo.dev@gmail.com>
2024-10-07 18:24:41 +0900

    Add 13610

---
+++ BaekjoonOJ/BruteForce/13610.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = read(), y = read(), z = y - x, i = 0;
+        while (z * i < y) i++;
+
+        bw.write(String.valueOf(i));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
