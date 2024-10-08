f673a0976ffa75a8fbf1246c3cb2b7f817bf3cb6
Jin <jinhyo.dev@gmail.com>
2024-10-08 14:13:10 +0900

    Add 3276

---
+++ BaekjoonOJ/BruteForce/3276.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), r = 1, c = 1;
+        while (r * c < n && ++r * c < n) c++;
+
+        bw.write(r + " " + c);
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
