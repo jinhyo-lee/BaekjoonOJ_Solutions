dd2afcf07ffbaa7e6ba38e4d01aa0eefda9dbf6f
Jin <jinhyo.dev@gmail.com>
2024-10-15 14:35:30 +0900

    Add 10886

---
+++ BaekjoonOJ/Implementation/10886.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = read(), cnt = 0;
+        while (n-- > 0) cnt = read() == 0 ? cnt - 1 : cnt + 1;
+        sb.append("Junhee is ").append(cnt > 0 ? "" : "not ").append("cute!");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
