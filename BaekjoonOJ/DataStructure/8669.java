f8ad5163d3ac65bf2f050a9dc231d8163a3d9bab
Jin <jinhyo.dev@gmail.com>
2024-10-23 17:29:59 +0900

    Add 8669

---
+++ BaekjoonOJ/DataStructure/8669.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<Integer> set = new HashSet<>();
+        int n = read();
+        while (n-- > 0) {
+            read();
+            set.add(read());
+        }
+
+        bw.write(String.valueOf(set.size()));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
