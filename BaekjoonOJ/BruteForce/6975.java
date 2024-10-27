0d3ab2e7467b8ec84af932f301f33443f5dd7470
Jin <jinhyo.dev@gmail.com>
2024-10-27 13:33:34 +0900

    Add 6975

---
+++ BaekjoonOJ/BruteForce/6975.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = read();
+        while (t-- > 0) {
+            int n = read(), sum = 0;
+            for (int i = 1; i < n; i++) if (n % i == 0) sum += i;
+            sb.append(n).append(" is ").append(sum < n ? "a deficient" : sum == n ? "a perfect" : "an abundant").append(" number.\n\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
