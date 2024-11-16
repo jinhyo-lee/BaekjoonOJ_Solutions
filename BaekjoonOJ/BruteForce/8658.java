824a71b4c9a9128f4dd8825f951a77053e59c6ce
Jin <jinhyo.dev@gmail.com>
2024-11-16 14:59:42 +0900

    Add 8658

---
+++ BaekjoonOJ/BruteForce/8658.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), i = 1, j = n;
+        while (++i < n) if (n % i != 0) break;
+        while (--j > 1) if (n % j != 0) break;
+
+        bw.write(i + " " + j);
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
