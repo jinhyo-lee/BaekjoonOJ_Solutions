d721f25d498020b94032f3178495ad78667833be
Jin <jinhyo.dev@gmail.com>
2024-11-30 14:55:41 +0900

    Add 7782

---
+++ BaekjoonOJ/BruteForce/7782.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean b = false;
+        int n = read(), b1 = read(), b2 = read();
+        while (!b && n-- > 0) b = read() <= b1 && read() <= b2 && b1 <= read() && b2 <= read();
+
+        bw.write(b ? "Yes" : "No");
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
