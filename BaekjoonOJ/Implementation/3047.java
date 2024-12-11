39827ce6c83cf1f07dbe1e619cb1783f291b4cc8
Jin <jinhyo.dev@gmail.com>
2024-12-11 16:10:45 +0900

    Add 3047

---
+++ BaekjoonOJ/Implementation/3047.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+        for (char c : br.readLine().toCharArray()) sb.append(arr[c == 'A' ? 0 : c == 'B' ? 1 : 2]).append(" ");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
