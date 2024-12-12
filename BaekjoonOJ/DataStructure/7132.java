71ad8979920ad01f84687e1e76c3ec8190bad705
Jin <jinhyo.dev@gmail.com>
2024-12-12 17:23:47 +0900

    Add 7132

---
+++ BaekjoonOJ/DataStructure/7132.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<Double> set = new HashSet<>();
+        int m = read(), n = read();
+        for (double i = m; i <= n; i++) for (double j = m; j <= n; j++) set.add(i / j);
+
+        bw.write(String.valueOf(set.size()));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
