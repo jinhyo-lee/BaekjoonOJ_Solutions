7ab2580ccc946b020cd998673e7a6973380ee5d5
Jin <jinhyo.dev@gmail.com>
2024-12-29 16:40:46 +0900

    Add 10179

---
+++ BaekjoonOJ/Implementation/10179.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) bw.write(String.format("$%.2f\n", Double.parseDouble(br.readLine()) * 0.8));
+
+        bw.flush();
+    }
+
+}
