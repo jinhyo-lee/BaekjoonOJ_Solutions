7be5a929e2cfbcd923fe2ac7d61653e70ac1b450
Jin <jinhyo.dev@gmail.com>
2025-01-24 09:39:30 +0900

    Add 8295

---
+++ BaekjoonOJ/BruteForce/8295.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), m = read(), p = read(), cnt = 0;
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++) if ((i + j) * 2 >= p) cnt += (n - i + 1) * (m - j + 1);
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
