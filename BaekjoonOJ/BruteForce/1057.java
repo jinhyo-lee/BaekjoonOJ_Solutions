4109f48aeff614e8200a0f8ba39354249cd6cb76
Jin <jinhyo.dev@gmail.com>
2025-02-10 11:08:45 +0900

    Add 1057

---
+++ BaekjoonOJ/BruteForce/1057.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        read();
+        int a = read(), b = read(), i = 0;
+        while (a != b) {
+            a -= a / 2;
+            b -= b / 2;
+            i++;
+        }
+
+        bw.write(String.valueOf(i));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
