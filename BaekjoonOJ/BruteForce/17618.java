b38bde3ff9be214beba0d7aba8e5ac87372efb97
Jin <jinhyo.dev@gmail.com>
2025-02-13 11:29:19 +0900

    Add 17618

---
+++ BaekjoonOJ/BruteForce/17618.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), cnt = 0;
+        while (n > 0) if (n % calc(n--) == 0) cnt++;
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int calc(int n) {
+        int sum = 0;
+        do sum += n % 10; while ((n /= 10) > 0);
+
+        return sum;
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
