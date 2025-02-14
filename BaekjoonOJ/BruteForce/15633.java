5e6f33453fdca53e1805e1780087b59954b531b1
Jin <jinhyo.dev@gmail.com>
2025-02-14 12:35:15 +0900

    Add 15633

---
+++ BaekjoonOJ/BruteForce/15633.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), sum = 0;
+        for (int i = 1; i <= n; i++) if (n % i == 0) sum += i;
+
+        bw.write(String.valueOf(sum * 5 - 24));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
