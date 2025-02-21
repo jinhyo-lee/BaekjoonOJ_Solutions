acf7ffa62cf410c6d9360b5fd8e6c0b66d2450b4
Jin <jinhyo.dev@gmail.com>
2025-02-21 09:30:39 +0900

    Add 1453

---
+++ BaekjoonOJ/Implementation/1453.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean[] arr = new boolean[101];
+        int n = read(), cnt = 0;
+        while (n-- > 0) {
+            int i = read();
+            if (arr[i]) cnt++;
+            else arr[i] = true;
+        }
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
