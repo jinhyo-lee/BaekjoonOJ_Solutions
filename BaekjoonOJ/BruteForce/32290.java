8a52084c03e36f0f4def2e8af16cf8b0fd70d66d
Jin <jinhyo.dev@gmail.com>
2025-03-06 10:15:25 +0900

    Add 32290

--- BaekjoonOJ/BruteForce/32290.java
+++ BaekjoonOJ/BruteForce/32290.java
@@ -7,15 +7,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int l = read(), r = read(), x = read();
-        int[] arr = new int[r - l + 1];
-        for (int i = 0; i < arr.length; i++) arr[i] = l++ | x;
+        int l = read();
+        int[] arr = new int[read() - l + 1];
+        for (int x = read(), i = 0; i < arr.length; i++) arr[i] = l++ | x;
 
-        bw.write(String.valueOf(MEX(arr)));
+        bw.write(String.valueOf(mex(arr)));
         bw.flush();
     }
 
-    private static int MEX(int[] arr) {
+    private static int mex(int[] arr) {
         if (arr[0] != 0) return 0;
         for (int i = 1; i < arr.length; i++) if (arr[i] - arr[i - 1] > 1) return arr[i - 1] + 1;
 
