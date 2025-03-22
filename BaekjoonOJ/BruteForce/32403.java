b1e16cfd5e574cde9d6e669354ad8bc3970c8b23
Jin <jinhyo.dev@gmail.com>
2025-03-22 14:25:05 +0900

    Add 32403

---
+++ BaekjoonOJ/BruteForce/32403.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), t = read(), sum = 0;
+        while (n-- > 0) {
+            int a = read(), cnt = 0;
+            while (t % (a + cnt) != 0 && (a - cnt < 1 || t % (a - cnt) != 0)) cnt++;
+            sum += cnt;
+        }
+
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
