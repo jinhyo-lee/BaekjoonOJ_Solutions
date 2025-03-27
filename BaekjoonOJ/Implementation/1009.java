70ab93482b965ad7079a62a2c09c66d6be4372da
Jin <jinhyo.dev@gmail.com>
2025-03-27 18:19:52 +0900

    Add 1009

---
+++ BaekjoonOJ/Implementation/1009.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = read();
+        while (t-- > 0) {
+            int a = read(), b = read(), n = 1;
+            while (b-- > 0) n = n * a % 10;
+            sb.append(n != 0 ? n : 10).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
