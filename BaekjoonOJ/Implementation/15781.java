347e34fbc33aafad68fdfe5125eb2caad4a16390
Jin <jinhyo.dev@gmail.com>
2025-03-29 12:28:08 +0900

    Add 15781

---
+++ BaekjoonOJ/Implementation/15781.java
@@ -0,0 +1,25 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), m = read(), a = 0, b = 0;
+        while (n-- > 0) a = Math.max(a, read());
+        while (m-- > 0) b = Math.max(b, read());
+
+        bw.write(String.valueOf(a + b));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
