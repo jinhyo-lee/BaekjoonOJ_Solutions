251549a0332d595d7cf596a1f2d4ce815adec628
Jin <jinhyo.dev@gmail.com>
2025-04-19 22:01:32 +0900

    Add 24081

---
+++ BaekjoonOJ/BruteForce/24081.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[read()];
+        int m = read();
+        for (int i = 0; i < arr.length; i++) arr[i] = read();
+
+        Set<Integer> set = new HashSet<>();
+        while (m-- > 0) set.add(read());
+
+        int cnt = 0;
+        for (int i : arr) if (set.contains(i)) cnt++;
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
