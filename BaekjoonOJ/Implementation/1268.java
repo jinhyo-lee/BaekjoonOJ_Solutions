7bd3ba19d45b1447d04a0225429c12a21338283a
Jin <jinhyo.dev@gmail.com>
2025-04-22 10:32:46 +0900

    Add 1268

---
+++ BaekjoonOJ/Implementation/1268.java
@@ -0,0 +1,47 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static int[][] mat;
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read();
+        mat = new int[n][5];
+        for (int i = 0; i < n; i++) for (int j = 0; j < 5; j++) mat[i][j] = read();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int[] tmp = new int[5];
+            System.arraycopy(mat[i], 0, tmp, 0, 5);
+            for (int j = 0; j < n; j++) if (j != i && search(tmp, j)) arr[i]++;
+        }
+
+        int max = 0, res = 0;
+        for (int i = 0; i < n; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+                res = i;
+            }
+        }
+
+        bw.write(String.valueOf(res + 1));
+        bw.flush();
+    }
+
+    private static boolean search(int[] tmp, int j) {
+        for (int k = 0; k < 5; k++) if (tmp[k] == mat[j][k]) return true;
+        return false;
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
