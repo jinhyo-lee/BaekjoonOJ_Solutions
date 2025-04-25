3b13184b69cd53a02c3eaa93c24dab04137f3c28
Jin <jinhyo.dev@gmail.com>
2025-04-25 09:02:11 +0900

    Add 5893

---
+++ BaekjoonOJ/Implementation/5893.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new BigInteger(br.readLine(), 2).multiply(BigInteger.valueOf(17)).toString(2));
+        bw.flush();
+    }
+
+}
