3951e6767e4813d83d33d2f4deb041095da0c619
Jin <jinhyo.dev@gmail.com>
2025-05-12 13:23:51 +0900

    Add 32585

---
+++ BaekjoonOJ/BruteForce/32585.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = read(), sum = 1;
+        while (n > 1) sum += n * (n-- + 1) / 2;
+
+        bw.write(String.valueOf(sum));
+        bw.flush();
+    }
+
+    private static long read() throws IOException {
+        long c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
