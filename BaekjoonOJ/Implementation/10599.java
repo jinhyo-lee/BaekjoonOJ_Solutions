9793a2a756d32383507d9632b65e372e61663f91
Jin <jinhyo.dev@gmail.com>
2025-05-24 13:41:52 +0900

    Add 10599

---
+++ BaekjoonOJ/Implementation/10599.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr;
+        while ((arr = new int[]{read(), read(), read(), read()})[0] != 0 || arr[3] != 0)
+            sb.append(arr[2] - arr[1]).append(' ').append(arr[3] - arr[0]).append('\n');
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
