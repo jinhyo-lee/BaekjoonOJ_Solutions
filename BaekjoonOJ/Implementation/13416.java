12cf9eaf10c00feee48c74fe20396f4259a2a3d5
Jin <jinhyo.dev@gmail.com>
2025-06-14 13:44:02 +0900

    Add 13416

---
+++ BaekjoonOJ/Implementation/13416.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = read();
+        while (t-- > 0) {
+            int n = read(), sum = 0, max;
+            while (n-- > 0) if ((max = Math.max(read(), Math.max(read(), read()))) > 0) sum += max;
+            sb.append(sum).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
