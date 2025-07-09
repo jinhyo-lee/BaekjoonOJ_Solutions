b12c5ffc657e29f40c7f322f4e8041fac386ef06
Jin <jinhyo.dev@gmail.com>
2025-07-09 09:42:15 +0900

    Add 2997

---
+++ BaekjoonOJ/Implementation/2997.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[]{read(), read(), read()};
+        Arrays.sort(arr);
+        int a = arr[1] - arr[0], b = arr[2] - arr[1];
+
+        bw.write(String.valueOf(a > b ? arr[0] + b : a < b ? arr[1] + a : arr[2] + a));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
