b801e576a420f0a2b26f956ce3bec340b9384e61
Jin <jinhyo.dev@gmail.com>
2025-08-05 14:43:29 +0900

    Add 28014

---
+++ BaekjoonOJ/Greedy/28014.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int cnt = 1;
+        for (int n = read(), i = read(), j; n > 1; n--, i = j) if (i <= (j = read())) cnt++;
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
