68b86e3c31d53b217d137d1cf5edabd7b54206a9
Jin <jinhyo.dev@gmail.com>
2025-08-12 16:16:04 +0900

    Add 17094

---
+++ BaekjoonOJ/Implementation/17094.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine()), a = 0, b = 0;
+        String s = br.readLine();
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '2') a++;
+            else b++;
+        }
+
+        bw.write(a == b ? "yee" : a > b ? "2" : "e");
+        bw.flush();
+    }
+
+}
