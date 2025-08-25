5c4daf9358030faca9f697e2ec8d8b2130b9112c
Jin <jinhyo.dev@gmail.com>
2025-08-25 18:01:29 +0900

    Add 5747

---
+++ BaekjoonOJ/Greedy/5747.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n;
+        while ((n = read()) != 0) {
+            int e = 0, o = 0;
+            for (int i = 0; i < n; i++) if ((read() & 1) == 0) e++;
+            while (n-- > 0) if ((read() & 1) != 0) o++;
+            sb.append(Math.abs(o - e)).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
