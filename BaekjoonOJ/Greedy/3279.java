739ced1cba4a6191bea8addf23cc67098dfd0c4b
Jin <jinhyo.dev@gmail.com>
2025-08-26 16:46:45 +0900

    Add 3279

---
+++ BaekjoonOJ/Greedy/3279.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read();
+        float sum = 100, i = read();
+        while (n-- > 1) {
+            float j = read();
+            if (i > j) sum *= i / j;
+            i = j;
+        }
+
+        bw.write(String.format("%.2f", sum));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
