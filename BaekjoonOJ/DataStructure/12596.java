edc8df2d3d097fb5105d0eb437c94b85277bf3cd
Jin <jinhyo.dev@gmail.com>
2025-09-10 16:05:53 +0900

    Add 12596

---
+++ BaekjoonOJ/DataStructure/12596.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        Set<Integer> set = new HashSet<>();
+        int n = read();
+        for (int t = 1; t <= n; t++) {
+            int c = read(), g;
+            while (c-- > 0) {
+                if (!set.contains(g = read())) set.add(g);
+                else set.remove(g);
+            }
+
+            sb.append("Case #").append(t).append(": ").append(set.iterator().next()).append('\n');
+            set.clear();
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
