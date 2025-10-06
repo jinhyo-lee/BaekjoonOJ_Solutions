dad7b83085e1e9b06887d54093c2444a2f5ee35e
Jin <jinhyo.dev@gmail.com>
2025-10-06 18:43:25 +0900

    Add 13163

---
+++ BaekjoonOJ/Implementation/13163.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            sb.append("god");
+            String s = br.readLine();
+            for (int i = s.indexOf(' '); i < s.length(); i++) if (s.charAt(i) != ' ') sb.append(s.charAt(i));
+            sb.append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
