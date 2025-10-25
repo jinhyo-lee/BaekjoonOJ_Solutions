ff7dce35e05db2f58991b0ad94da6fab8716f067
Jin <jinhyo.dev@gmail.com>
2025-10-25 13:32:48 +0900

    Add 31448

---
+++ BaekjoonOJ/Greedy/31448.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), i = read(), j = i, max = 0;
+        while (n-- > 1) {
+            int k = read();
+            if (j < k) max = Math.max(max, k - i);
+            else i = k;
+            j = k;
+        }
+
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
