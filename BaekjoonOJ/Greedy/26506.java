85ebe07bd2a7ee23ddfc8a19ad9d194d0df018c6
Jin <jinhyo.dev@gmail.com>
2025-11-20 13:06:32 +0900

    Add 26506

---
+++ BaekjoonOJ/Greedy/26506.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[read()];
+        for (int i = 0; i < arr.length; i++) arr[i] = read();
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+        for (int i = arr.length / 2 - 1; i >= 0; i--) min = Math.min(min, arr[i] + arr[arr.length - i - 1]);
+
+        bw.write(String.valueOf(min));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
