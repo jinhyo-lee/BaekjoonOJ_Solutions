cb5c4a0d6847011189b655636749b2e7619545e8
Jin <jinhyo.dev@gmail.com>
2025-11-22 15:48:15 +0900

    Add 3460

---
+++ BaekjoonOJ/Implementation/3460.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = read();
+        while (t-- > 0) {
+            String n = Integer.toBinaryString(read());
+            int l = n.length();
+            for (int i = l - 1; i >= 0; i--) if (n.charAt(i) == '1') sb.append(l - i - 1).append(' ');
+            sb.append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
