3e7e9ced5d63f92485e29a1438e78644cb398d35
Jin <jinhyo.dev@gmail.com>
2025-12-13 22:15:03 +0900

    Add 1259

---
+++ BaekjoonOJ/Implementation/1259.java
@@ -0,0 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n;
+        while ((n = read()) != 0) sb.append(isPalindrome(String.valueOf(n)) ? "yes\n" : "no\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static boolean isPalindrome(String s) {
+        int n = s.length() / 2;
+        for (int i = 0; i < n; i++) if (s.charAt(i) != s.charAt(s.length() - i - 1)) return false;
+
+        return true;
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
