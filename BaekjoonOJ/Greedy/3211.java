809ee2958dda844e7291ec335b69ef9af755084d
Jin <jinhyo.dev@gmail.com>
2025-12-20 14:33:35 +0900

    Add 3211

---
+++ BaekjoonOJ/Greedy/3211.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[read()];
+        for (int i = 0; i < arr.length; i++) arr[i] = read();
+        Arrays.sort(arr);
+
+        int i = 0;
+        while (i < arr.length) if (arr[i] < ++i) break;
+
+        bw.write(String.valueOf(i));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
