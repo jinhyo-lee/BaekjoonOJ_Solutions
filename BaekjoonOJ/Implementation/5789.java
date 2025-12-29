93c5f0b15ce6f32ddf672476c26bfe860ac08308
Jin <jinhyo.dev@gmail.com>
2025-12-29 14:09:40 +0900

    Add 5789

---
+++ BaekjoonOJ/Implementation/5789.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            String s = br.readLine();
+            sb.append("Do-it").append(s.charAt(s.length() / 2) == s.charAt(s.length() / 2 - 1) ? '\n' : "-Not\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
