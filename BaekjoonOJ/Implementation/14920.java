bec615ada52d5af166a680189afc4ab1572a3cad
Jin <jinhyo.dev@gmail.com>
2025-12-30 14:17:05 +0900

    Add 14920

---
+++ BaekjoonOJ/Implementation/14920.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int c = read(), n = 1;
+        while (c != 1) {
+            c = ((c & 1) == 0) ? c / 2 : 3 * c + 1;
+            n++;
+        }
+
+        bw.write(String.valueOf(n));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
