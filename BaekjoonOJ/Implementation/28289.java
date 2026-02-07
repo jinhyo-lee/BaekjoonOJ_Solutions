5350a84c7994406e939c9089dca681b13d3ff512
Jin <jinhyo.dev@gmail.com>
2026-02-07 12:25:36 +0900

    Add 28289

---
+++ BaekjoonOJ/Implementation/28289.java
@@ -0,0 +1,34 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[4];
+        int p = read();
+        while (p-- > 0) {
+            int g = read(), c = read();
+            if (g > 1) arr[c == 1 || c == 2 ? 0 : c == 3 ? 1 : 2]++;
+            else arr[3]++;
+            read();
+        }
+
+        for (int i : arr) sb.append(i).append('\n');
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+
+}
