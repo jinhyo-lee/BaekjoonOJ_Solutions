1793cf21a1ba7c896393ad6c37073be8c1dd9f0e
Jin <jinhyo.dev@gmail.com>
2026-02-21 22:10:14 +0900

    Add 6438

---
+++ BaekjoonOJ/Implementation/6438.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        while (n-- > 0) {
+            String s = br.readLine();
+            for (int i = s.length() - 1; i >= 0; i--) sb.append(s.charAt(i));
+            sb.append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
