803f8635944a98e7047dc2127bc4fb5bc5458a51
Jin <jinhyo.dev@gmail.com>
2026-03-01 14:26:04 +0900

    Add 14761

---
+++ BaekjoonOJ/Implementation/14761.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int x = read(), y = read(), n = read();
+        for (int i = 1; i <= n; i++)
+            sb.append(i % x == 0 && i % y == 0 ? "FizzBuzz" : i % x == 0 ? "Fizz" : i % y == 0 ? "Buzz" : i).append('\n');
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
