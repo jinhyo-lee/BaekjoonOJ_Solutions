d3462c0af37580f6cb621207b224bc620d57e7d1
Jin <jinhyo.dev@gmail.com>
2026-03-14 13:29:07 +0900

    Add 13236

---
+++ BaekjoonOJ/Implementation/13236.java
@@ -0,0 +1,26 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = read();
+        sb.append(n).append(' ');
+        while (n != 1) sb.append(n = (n & 1) == 0 ? n / 2 : n * 3 + 1).append(' ');
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
