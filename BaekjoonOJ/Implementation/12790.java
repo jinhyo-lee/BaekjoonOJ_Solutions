b601d6576602642cf43b899e3d0c290ae049f0a8
Jin <jinhyo.dev@gmail.com>
2026-03-21 11:03:13 +0900

    Add 12790

---
+++ BaekjoonOJ/Implementation/12790.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int t = read();
+        while (t-- > 0) {
+            int a = read(), b = read(), c = read(), d = read();
+            sb.append(Math.max(1, a + read()) + 5 * Math.max(1, b + read()) + 2 * Math.max(0, c + read()) + 2 * (d + read())).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
