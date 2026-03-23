a2484c430468abfb91f8265c6d09794492b63b11
Jin <jinhyo.dev@gmail.com>
2026-03-23 22:36:01 +0900

    Add 10818

---
+++ BaekjoonOJ/Implementation/10818.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), i = read(), min = i, max = i;
+        while (n-- > 1) {
+            min = Math.min(min, i = read());
+            max = Math.max(max, i);
+        }
+
+        bw.write(min + " " + max);
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        boolean flag = n == 13;
+
+        if (flag) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return flag ? ~n + 1 : n;
+    }
+
+}
