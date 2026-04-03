9e0c1fbee878dff09aa7e780a1ec869bfc21371c
Jin <jinhyo.dev@gmail.com>
2026-04-03 22:06:38 +0900

    Add 2954

---
+++ BaekjoonOJ/Implementation/2954.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String s = br.readLine();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') i += 2;
+            sb.append(c);
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+}
