3823d77b88797100fd8778c637f2ffd9a5d71626
Jin <jinhyo.dev@gmail.com>
2026-04-09 20:34:26 +0900

    Add 8714

---
+++ BaekjoonOJ/Implementation/8714.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = read(), cnt = 0;
+        for (int i = 0; i < n; i++) if (read() == 0) cnt++;
+
+        bw.write(String.valueOf(Math.min(cnt, n - cnt)));
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
+}
